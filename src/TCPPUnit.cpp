2c20110209e415a68a4b68e264a685996c1313f7
Scott R Charlton <charlton@usgs.gov>
2011-05-14 06:46:14 +0000

    all non-interactive tests pass
    git-svn-id: svn://136.177.114.72/svn_GW/WPhast/trunk@5404 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- src/TCPPUnit.cpp
+++ src/TCPPUnit.cpp
@@ -70,18 +70,18 @@ void TCPPUnit::RunUnitTests(void)
 		double d1 = 0/z;
 #endif
 
-// COMMENT: {10/20/2010 4:28:42 PM}		runner.addTest(TestCPhastInput::suite());
-// COMMENT: {10/20/2010 4:28:42 PM}// COMMENT: {8/18/2010 7:04:57 PM}		runner.addTest(TestCZone::suite());
-// COMMENT: {10/20/2010 4:28:42 PM}		runner.addTest(TestCGrid::suite());
+		runner.addTest(TestCPhastInput::suite());
+		runner.addTest(TestCZone::suite());
+		runner.addTest(TestCGrid::suite());
 		runner.addTest(TestPHAST_Transform::suite());
-// COMMENT: {8/18/2010 7:05:00 PM}		runner.addTest(TestCtime::suite());
-// COMMENT: {8/18/2010 7:05:00 PM}		runner.addTest(TestCBC::suite());
-// COMMENT: {8/18/2010 7:05:00 PM}		runner.addTest(TestCTimeControl::suite());
-// COMMENT: {8/18/2010 7:05:00 PM}		runner.addTest(TestCWPhastDoc::suite());
-// COMMENT: {8/18/2010 7:05:00 PM}		runner.addTest(TestCParser::suite());
-// COMMENT: {8/18/2010 7:05:00 PM}		runner.addTest(TestCPropertyTreeControlBar::suite());
-// COMMENT: {8/18/2010 7:05:00 PM}		runner.addTest(TestCUnits::suite());
-// COMMENT: {8/18/2010 7:05:00 PM}		runner.addTest(TestCPhastInput::suite());
+		runner.addTest(TestCtime::suite());
+		runner.addTest(TestCBC::suite());
+		runner.addTest(TestCTimeControl::suite());
+		runner.addTest(TestCWPhastDoc::suite());
+		runner.addTest(TestCParser::suite());
+		runner.addTest(TestCPropertyTreeControlBar::suite());
+		runner.addTest(TestCUnits::suite());
+		runner.addTest(TestCPhastInput::suite());
 		runner.addTest(TestPrism::suite());
 		runner.addTest(TestHDFSerializePolyData::suite());
 		runner.addTest(TestUniqueMap::suite());
