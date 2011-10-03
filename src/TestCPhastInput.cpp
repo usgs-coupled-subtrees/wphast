303b03260d2a7051355bd78d94aa63f3902050c0
Scott R Charlton <charlton@usgs.gov>
2011-10-03 19:39:09 +0000

    errors are now warnings; adjusted unit tests accordingly
    git-svn-id: svn://136.177.114.72/svn_GW/WPhast/trunk@5670 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- src/TestCPhastInput.cpp
+++ src/TestCPhastInput.cpp
@@ -2794,7 +2794,8 @@ void TestCPhastInput::testRadial(void)
 				TRACE("%s\n", pInput->GetWarningMsg());
 				TRACE("%s\n", pInput->GetErrorMsg());
 			}
-			CPPUNIT_ASSERT(pInput->GetErrorCount()   == 8);
+			CPPUNIT_ASSERT(pInput->GetErrorCount()   == 0);
+			CPPUNIT_ASSERT(pInput->GetWarningCount() == 3);
 
 			pInput->Delete();
 			pInput = NULL;
